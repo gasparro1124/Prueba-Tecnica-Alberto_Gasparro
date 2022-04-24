f58ee061a5147719772f842d6998e95a18a5e4b1
gasparro1124 <albgas.bon@gmail.com>
2022-04-24 14:23:16 +0200

    second commit

--- src/app/pages/miniatures/miniatures.component.ts
+++ src/app/pages/miniatures/miniatures.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { of, Observable } from 'rxjs';
 import { UserResponse } from '../interfaces';
 
 @Component({
@@ -8,11 +9,39 @@ import { UserResponse } from '../interfaces';
 })
 export class MiniaturesComponent implements OnInit {
 
-  @Input() users!: UserResponse[]
+  @Input() users:UserResponse[] = []
+
+  usersMarketing:UserResponse[]= []
+  usersDevelopment:UserResponse[]= []
+
+  usersMarketingObserver:Observable<UserResponse[]>=of(this.users)
+  usersDevelopmentObserver:Observable<UserResponse[]>=of(this.users)
 
   constructor() { }
 
   ngOnInit(): void {
+
+    this.usersMarketingObserver.subscribe( r => {
+      console.log(r)
+      r.filter( user => {
+        if(user.department.toLowerCase() == 'marketing')
+          this.usersMarketing.push(user)
+      })
+    })
+
+    this.usersDevelopmentObserver.subscribe( r => {
+      r.filter( user => {
+        if(user.department.toLowerCase() == 'development')
+          this.usersMarketing.push(user)
+      })
+    })
+
+  }
+
+  searchUser(members:UserResponse[], value:String): UserResponse[] {
+    return members.filter(
+      member => member.name.toLowerCase().includes(value.toLowerCase()) ||  member.email.toLowerCase().includes(value.toLowerCase())
+    )
   }
 
 }
